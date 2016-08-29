2da099db5c8419c83a1079beaca589df4a293c9b
Noel Ferreira <noel.ferreira@gmail.com>
2016-08-28 21:11:50 -0400

    First Swift Try

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -13,11 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.jpg")!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showMessage(){
+        
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([.Day, .Month, .Year], fromDate: date)
+        
+        let year = String(components.year)
+        let month = String(components.month)
+        let day = String(components.day)
+        
+        let deviceName = UIDevice.currentDevice().name
+        
+        UIDevice.currentDevice().batteryMonitoringEnabled = true
+        let batteryLevel = "Nível da bateria: " + String(Int(UIDevice.currentDevice().batteryLevel)*100) + "%"
+        
+        let alertController = UIAlertController(title: "Bem-Vindo ao meu novo aplicativo", message: "Data de hoje no " + deviceName + " : " + day + "/" + month + "/" + year + "\n" + batteryLevel, preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
 
 
